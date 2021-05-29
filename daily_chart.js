679e5aea3edb1a70101d4cad4a85db6b36d2f382
hell0win <gopooop@gmail.com>
2021-05-30 01:01:16 +0300

    Update daily_chart.js

--- daily_chart.js
+++ daily_chart.js
@@ -4,7 +4,7 @@ let min_price_data = d.data.map(function (item) {
 let max_price_data = d.data.map(function (item) {
   return [item.t, item.p_max]
 });
-return {
+{
   xAxis: { type: 'time', min: 'dataMin', max: 'dataMax'},
   yAxis: [
       { type: 'value', min: 'dataMin', max: 'dataMax'},
